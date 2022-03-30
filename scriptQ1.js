f9102161d9f367519d6cb2e951cf05b4489d233d
premkumar0331 <84247024+premkumar0331@users.noreply.github.com>
2022-03-30 19:13:47 +0530

    Update scriptQ1.js

--- scriptQ1.js
+++ scriptQ1.js
@@ -1,14 +1,19 @@
-const obj1 = {
-    "name": "prem Kumar",
-    "age": 30
+var obj1 = { name: "Person 1", age:5 };
+var obj2 = { age:5, name: "Person 1" };
+let equal = false;
+function issame(obj1,obj2){
+    let obj1keys = Object.keys(obj1);
+    let obj2keys = Object.keys(obj2);
+    if(obj1keys.length == obj2keys.length){
+        for(let key in obj1){
+            if(obj1[key]==obj2[key]){
+                equal = true;
+            }else{
+                equal = false;
+                break;
+            }
+        }
+    }
+return equal;
 }
-
-const obj2 = {
-    "age": 30,
-    "name": "prem Kumar"
-
-}
-
-const isequal = JSON.stringify(obj1)===JSON.stringify(obj2);
-
-console.log(isequal);
+console.log(issame(obj1,obj2));
